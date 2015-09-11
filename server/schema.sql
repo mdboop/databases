1466fa8fd2e410c1f3ebc9dc9038be2f0a7dca69
mdboop <mdeanbrooks@gmail.com>
2015-09-11 16:03:52 -0700

    Describe tables in schema

--- server/schema.sql
+++ server/schema.sql
@@ -2,12 +2,26 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-);
-
 /* Create other tables and define schemas for them here! */
 
+CREATE TABLE messages (
+  /* Describe your table here.*/
+  id int(5),
+  message varchar(255),
+  time varchar(15), 
+  user_id int(5),
+  room_id int(5)
+);
+
+CREATE TABLE users (
+  id int(5), 
+  name varchar(20)
+);
+
+CREATE TABLE rooms (
+  id int(5), 
+  room varchar(20)
+);
 
 
 
